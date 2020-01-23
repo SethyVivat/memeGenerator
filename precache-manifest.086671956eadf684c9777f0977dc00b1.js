0ab9e2e1df564a1af812a9153ef0815c694cc9ff
SethyVivat <sethyvivat67@gmail.com>
2020-01-23 08:58:30 +0700

    Updates

---
+++ precache-manifest.086671956eadf684c9777f0977dc00b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8d7f62b6cd332b553cfbe925bc75bc",
+    "url": "/memeGenerator/index.html"
+  },
+  {
+    "revision": "35169a5658de602ec048",
+    "url": "/memeGenerator/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4be41a7ad1917f8182f5",
+    "url": "/memeGenerator/static/css/main.79c11c02.chunk.css"
+  },
+  {
+    "revision": "35169a5658de602ec048",
+    "url": "/memeGenerator/static/js/2.3c54dca1.chunk.js"
+  },
+  {
+    "revision": "4be41a7ad1917f8182f5",
+    "url": "/memeGenerator/static/js/main.af3ef424.chunk.js"
+  },
+  {
+    "revision": "1fb97b7f598f5254b6ad",
+    "url": "/memeGenerator/static/js/runtime-main.1e2d56ce.js"
+  }
+]);
